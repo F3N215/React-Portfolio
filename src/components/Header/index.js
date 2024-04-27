1d53be24b180b227ea45329c53f32b3c30f1ca67
Daniel John <dcampo85@gmail.com>
2024-04-27 16:45:48 -0400

    troubleshooting

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,7 +9,7 @@ function Header(props) {
       <section>
         <header className="flex-row px-1">
           <div>
-            <h1>Lindsey Martin's Portfolio</h1>
+            <h1>Daniel John's Portfolio</h1>
           </div>
           <div>
             <Nav
