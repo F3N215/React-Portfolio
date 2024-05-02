66779bbc2dca0c7330b85c35dc7101a27a4cd3ea
Daniel John <dcampo85@gmail.com>
2024-05-02 08:24:53 -0400

    deploy test #10

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,7 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: "index.js",
+      plugins: [react()],
+    },
+  },
   server: {
     port: 3000,
     open: true,
