b3631f8691e242b24e10797330260878d1848f56
Daniel John <dcampo85@gmail.com>
2024-05-02 09:41:11 -0400

    revise footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,67 @@
+// Importing necessary icons from react-icons library
+import { SiGithub, SiLinkedin, SiInstagram } from "react-icons/si";
+
+// Getting the current year
+const currentYear = new Date().getFullYear();
+
+// Define FooterWithSocialLinks functional component
+export function FooterWithSocialLinks() {
+  // Return JSX for FooterWithSocialLinks component
+  return (
+    <footer className="fixed bottom-0 left-0 right-0 flex justify-between items-center mx-auto p-7 text-lg w-full z-10">
+      {/* Left section containing name and copyright */}
+      <div className="flex items-center text-black">
+        {/* Link to GitHub profile */}
+        <a
+          href="https://github.com/F3N215"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-black hover:text-accent"
+        >
+          Daniel John
+        </a>
+        {/* Display current year */}
+        <div className="ml-2 text-xs flex items-center">
+          <span>&copy; {currentYear}</span>
+          <span className="heart-symbol text-accent text-sm ml-1 animate__animated animate__heartBeat animate__slow animate__infinite">
+            &#x2665;
+          </span>
+          <span className="font-bold ml-1">M</span>
+        </div>
+      </div>
+      {/* Right section containing social links */}
+      <div className="flex gap-4">
+        {/* Link to LinkedIn profile */}
+        <a
+          href="https://www.linkedin.com/in/dan-campo"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-myaccent hover:text-accent"
+          title="Linkedin Profile"
+        >
+          <SiLinkedin size={36} />
+        </a>
+        {/* Link to Frontend Mentor profile */}
+        <a
+          href="https://www.instagram.com/direpikeblackandwhite"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-myaccent hover:text-accent"
+          title="Instagram Profile"
+        >
+          <SiInstagram size={36} />
+        </a>
+        {/* Link to GitHub profile */}
+        <a
+          href="https://github.com/F3N215"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-myaccent hover:text-accent"
+          title="Github Profile"
+        >
+          <SiGithub size={36} />
+        </a>
+      </div>
+    </footer>
+  );
+}
--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: "index.js",
+      input: "./src/main.jsx",
       plugins: [react()],
     },
   },
